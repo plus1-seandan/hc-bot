472a17ced51b8010d25034bb774eeaa310b3cf2a
plus1-seandan <seandan@Seans-MBP.attlocal.net>
2023-02-28 17:30:43 -0600

    add message

--- src/index.js
+++ src/index.js
@@ -45,32 +45,16 @@ app.get("/webhook", (req, res) => {
 
 // Creates the endpoint for our webhook
 app.post("/webhook", (req, res) => {
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === "page") {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-      let sender = webhook_event.sender.id;
-      if (webhook_event.message && webhook_event.message.text) {
-        let text = webhook_event.message.text;
-        sendTextMessage(
-          sender,
-          "Text received, echo: " + text.substring(0, 200)
-        );
-      }
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED");
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
+  let messaging_events = req.body.entry[0].messaging;
+  for (let i = 0; i < messaging_events.length; i++) {
+    let event = req.body.entry[0].messaging[i];
+    let sender = event.sender.id;
+    if (event.message && event.message.text) {
+      let text = event.message.text;
+      sendTextMessage(sender, prayer_request);
+    }
   }
+  res.sendStatus(200);
 });
 
 function sendTextMessage(sender, text) {
@@ -94,3 +78,15 @@ function sendTextMessage(sender, text) {
     }
   );
 }
+
+const prayer_request = `Abel Kim: Thankful for meal with siblings, week has been taxing, not sleeping well, issues with clenching teeth at night. Sister's dog staying over; can be loud. PR: for car and sleep situation to get figured out \n
+Daniel Kwak: Thankful for meet ups with Sean, Sam; relationship depends on day by day but doing okay. Felt tired, mental health not great, rough week. Feeling shame, hard to talk about things that don't go right, feeling stressed on next steps, uncomfortable to share. PR: to get sleep and inner peace \n
+Gabriel Ra: Thankful for better sleep, felt stressed about interviews for internship; not too anxious but not at peace. Really busy and tiring with school. PR: for better sleep, for 2 interviews on Monday.\n
+Hana Park: Work has been bad, still feeling lazy but at a better headspace with relationships and accepting where she is. Good reminder from preparing testimony, lots of little joys this week, want to build better habits. PR: to build good habits\n
+Maria Kim: X\n
+Noah Choe: Thankful for Monday off, hanging out with people. Feeling confused; still not on a project and lowkey anxious but trying to trust, questioning what God has in store. PR: to get on a project, more discipline on devotionals and work life \n
+Odelia Kim: to do well on her procedure on patient tomorrow and for her to recover from being sick \n
+Sean Dan: Thankful for the ways that God work, feeling healing from mental health, realizing the need for Jesus. Work has been very not busy, spending more time with people, been waking up earlier and gets easier. PR: waking up earlier\n
+Tiffany Jeong: Thankful for spending time w people, felt good overall but feeling sad when people count John and i on a different boat relationship wise, PR: to find a dress I love tmrw!!\n
+Timothy Cho: X\n
+Zach Munder: Thankful for hosting HC this week, haven't heard back from internships yet, very nerve wrecking, not feeling motivated with school. PR: to hear back from internships`;
